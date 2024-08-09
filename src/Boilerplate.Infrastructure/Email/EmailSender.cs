996f6c5f70375868a5fa96c7da39af670fd6cb06
mooronsi <work@mooronsi.dev>
2024-08-09 17:36:14 +0300

    feature(Auth): Added EmailSender for Identity

---
+++ src/Boilerplate.Infrastructure/Email/EmailSender.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.Threading.Tasks;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace Boilerplate.Infrastructure.Email;
+
+public class EmailSender(ILogger<EmailSender> logger, IOptions<EmailSettings> emailSettings) : IEmailSender
+{
+    private readonly EmailSettings _emailSettings = emailSettings.Value;
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        logger.LogInformation("Sending email to |{to}| from |{from}| with subject |{subject}|",
+            email, _emailSettings.FromEmail, subject);
+
+        try
+        {
+            MimeMessage mailMessage = new();
+
+            mailMessage.From.Add(new MailboxAddress(_emailSettings.FromName, _emailSettings.FromEmail));
+            mailMessage.To.Add(new MailboxAddress(email, email));
+
+            mailMessage.Subject = subject;
+            mailMessage.Body = new TextPart(TextFormat.Html) { Text = htmlMessage };
+
+            SmtpClient client = new();
+            await client.ConnectAsync(_emailSettings.SmtpHost, _emailSettings.SmtpPort);
+            await client.AuthenticateAsync(_emailSettings.FromEmail, _emailSettings.FromPassword);
+
+            await client.SendAsync(mailMessage);
+
+            await client.DisconnectAsync(true);
+
+            logger.LogInformation("Email sent to |{to}| from |{from}| with subject |{subject}|",
+                email, _emailSettings.FromEmail, subject);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error while sending email: {message}", ex.Message);
+            throw;
+        }
+    }
+}
